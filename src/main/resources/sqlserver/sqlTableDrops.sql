0f0b7d89ec850e64473748574f53d0c51880887c
Daniel R <daniel.rincon96@gmail.com>
2021-04-11 16:14:49 -0500

    SQL Server implementation

---
+++ src/main/resources/sqlserver/sqlTableDrops.sql
@@ -0,0 +1,21 @@
+-- Drops all tables and schema.
+
+drop table benchmarksql.warehouse;
+
+drop table benchmarksql.item;
+
+drop table benchmarksql.stock;
+
+drop table benchmarksql.district;
+
+drop table benchmarksql.customer;
+
+drop table benchmarksql.oorder;
+
+drop table benchmarksql.order_line;
+
+drop table benchmarksql.history;
+
+drop table benchmarksql.new_order;
+
+DROP SCHEMA IF EXISTS benchmarksql;
---
+++ src/main/resources/sqlserver/sqlTableTruncates.sql
@@ -0,0 +1,19 @@
+-- Performs a truncation of all tables. Faster that a delete.
+
+truncate table benchmarksql.warehouse;
+
+truncate table benchmarksql.item;
+
+truncate table benchmarksql.stock;
+
+truncate table benchmarksql.district;
+
+truncate table benchmarksql.customer;
+
+truncate table benchmarksql.history;
+
+truncate table benchmarksql.oorder;
+
+truncate table benchmarksql.order_line;
+
+truncate table benchmarksql.new_order;
